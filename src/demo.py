243b9dd09901aabc07f699815264581c4f3a05b9
jacksonlli <59549394+jacksonlli@users.noreply.github.com>
2021-04-03 17:10:47 -0400

    write bounding boxes to file

--- src/demo.py
+++ src/demo.py
@@ -19,90 +19,99 @@ video_ext = ['mp4', 'mov', 'avi', 'mkv']
 time_stats = ['tot', 'load', 'pre', 'net', 'dec', 'post', 'merge', 'display']
 
 def demo(opt):
-  os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpus_str
-  opt.debug = max(opt.debug, 1)
-  detector = Detector(opt)
+	os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpus_str
+	opt.debug = max(opt.debug, 1)
+	detector = Detector(opt)
 
-  if opt.demo == 'webcam' or \
-    opt.demo[opt.demo.rfind('.') + 1:].lower() in video_ext:
-    is_video = True
-    # demo on video stream
-    cam = cv2.VideoCapture(0 if opt.demo == 'webcam' else opt.demo)
-  else:
-    is_video = False
-    # Demo on images sequences
-    if os.path.isdir(opt.demo):
-      image_names = []
-      ls = os.listdir(opt.demo)
-      for file_name in sorted(ls):
-          ext = file_name[file_name.rfind('.') + 1:].lower()
-          if ext in image_ext:
-              image_names.append(os.path.join(opt.demo, file_name))
-    else:
-      image_names = [opt.demo]
+	if opt.demo == 'webcam' or \
+	opt.demo[opt.demo.rfind('.') + 1:].lower() in video_ext:
+	is_video = True
+	# demo on video stream
+	cam = cv2.VideoCapture(0 if opt.demo == 'webcam' else opt.demo)
+	else:
+	is_video = False
+	# Demo on images sequences
+	if os.path.isdir(opt.demo):
+	  image_names = []
+	  ls = os.listdir(opt.demo)
+	  for file_name in sorted(ls):
+		  ext = file_name[file_name.rfind('.') + 1:].lower()
+		  if ext in image_ext:
+			  image_names.append(os.path.join(opt.demo, file_name))
+	else:
+	  image_names = [opt.demo]
 
-  # Initialize output video
-  out = None
-  out_name = opt.demo[opt.demo.rfind('/') + 1:]
-  print('out_name', out_name)
-  if opt.save_video:
-    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    fourcc = cv2.VideoWriter_fourcc(*'H264')
-    out = cv2.VideoWriter('../results/{}.mp4'.format(
-      opt.exp_id + '_' + out_name),fourcc, opt.save_framerate, (
-        opt.video_w, opt.video_h))
-  
-  if opt.debug < 5:
-    detector.pause = False
-  cnt = 0
-  results = {}
+	# Initialize output video
+	out = None
+	out_name = opt.demo[opt.demo.rfind('/') + 1:]
+	print('out_name', out_name)
+	if opt.save_video:
+	# fourcc = cv2.VideoWriter_fourcc(*'XVID')
+	fourcc = cv2.VideoWriter_fourcc(*'H264')
+	out = cv2.VideoWriter('../results/{}.mp4'.format(
+	  opt.exp_id + '_' + out_name),fourcc, opt.save_framerate, (
+		opt.video_w, opt.video_h))
 
-  while True:
-      if is_video:
-        _, img = cam.read()
-        if img is None:
-          save_and_exit(opt, out, results, out_name)
-      else:
-        if cnt < len(image_names):
-          img = cv2.imread(image_names[cnt])
-        else:
-          save_and_exit(opt, out, results, out_name)
-      cnt += 1
+	if opt.debug < 5:
+	detector.pause = False
+	cnt = 0
+	results = {}
+	with open("results.txt", "w") as text_file:
+		while True:
+			if is_video:
+			_, img = cam.read()
+			if img is None:
+			  save_and_exit(opt, out, results, out_name)
+			else:
+			if cnt < len(image_names):
+			  img = cv2.imread(image_names[cnt])
+			else:
+			  save_and_exit(opt, out, results, out_name)
+			cnt += 1
 
-      # resize the original video for saving video results
-      if opt.resize_video:
-        img = cv2.resize(img, (opt.video_w, opt.video_h))
+			# resize the original video for saving video results
+			if opt.resize_video:
+			img = cv2.resize(img, (opt.video_w, opt.video_h))
 
-      # skip the first X frames of the video
-      if cnt < opt.skip_first:
-        continue
-      
-      #cv2.imshow('input', img)
+			# skip the first X frames of the video
+			if cnt < opt.skip_first:
+			continue
 
-      # track or detect the image.
-      ret = detector.run(img)
+			#cv2.imshow('input', img)
 
-      # log run time
-      time_str = 'frame {} |'.format(cnt)
-      for stat in time_stats:
-        time_str = time_str + '{} {:.3f}s |'.format(stat, ret[stat])
-      print(time_str)
+			# track or detect the image.
+			ret = detector.run(img)
 
-      # results[cnt] is a list of dicts:
-      #  [{'bbox': [x1, y1, x2, y2], 'tracking_id': id, 'category_id': c, ...}]
-      results[cnt] = ret['results']
+			# log run time
+			time_str = 'frame {} |'.format(cnt)
+			for stat in time_stats:
+			time_str = time_str + '{} {:.3f}s |'.format(stat, ret[stat])
+			print(time_str)
 
-      # save debug image to video
-      if opt.save_video:
-        out.write(ret['generic'])
-        if not is_video:
-          cv2.imwrite('../results/demo{}.jpg'.format(cnt), ret['generic'])
-      
-      # esc to quit and finish saving video
-      if cv2.waitKey(1) == 27:
-        save_and_exit(opt, out, results, out_name)
-        return 
-  save_and_exit(opt, out, results)
+			# results[cnt] is a list of dicts:
+			#  [{'bbox': [x1, y1, x2, y2], 'tracking_id': id, 'category_id': c, ...}]
+			results[cnt] = ret['results']
+			for frame_objects in results[cnt]:
+				for obj_dict in frame_objects:
+					if obj_dict["class"]==42:#cup
+						bbarray = obj_dict['bbox']
+						t_id = obj_dict['tracking_id']
+						if t_id == 1:#to associate the id from the init.txt file to the ids found here
+							t_id = 2
+						elif t_id == 2:
+							t_id = 1
+						print(f"{cnt} {t_id} {bbarray[0]} {bbarray[2]} {bbarray[1]} {bbarray[3]}", file=text_file)
+			# save debug image to video
+			if opt.save_video:
+			out.write(ret['generic'])
+			if not is_video:
+			  cv2.imwrite('../results/demo{}.jpg'.format(cnt), ret['generic'])
+
+			# esc to quit and finish saving video
+			if cv2.waitKey(1) == 27:
+			save_and_exit(opt, out, results, out_name)
+			return 
+	save_and_exit(opt, out, results)
 
 
 def save_and_exit(opt, out=None, results=None, out_name=''):
